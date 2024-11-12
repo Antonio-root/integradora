7b692c4cffc87f6ea6c1df9f6bdf4b3665478685
AntonioPC <juancastillo2202@gmail.com>
2024-11-11 23:16:00 -0600

    se agrego mas estilo a index y redireccionamientos

---
+++ comunidad.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comunidad</title>
+</head>
+<body>
+    <h1>Aqui se va el chat global de la comuniad osea puro backend</h1>
+    <h2>Separlarlo por sections publicaciones en medio, trends por un lado, noticias al lado tambien (derecha)</h2>
+    <h3>shortcuts en una section en la izquierda</h3>
+</body>
+</html>

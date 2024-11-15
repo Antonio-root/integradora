b8c482770e1dc7fa235d4b6592e892de63e8c174
julisacid <yp4alma@gmail.com>
2024-11-14 22:53:20 -0600

    se modifico estilo a login

--- login.php
+++ login.php
@@ -67,49 +67,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio de sesion</title>
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    
+    ><link rel="stylesheet" href="styles4.css">
 
-    <style>
-
-        header{
-          display: flex;
-          text-align: center;
-          background-color: rgb(19, 110, 39);
-          font-style: italic;
-          color: rgb(219, 219, 10);
-        }
-        @keyframes rgb-border {
-        0% {
-          border-color: red;
-        }
-        33% {
-          border-color: green;
-        }
-        66% {
-          border-color: blue;
-        }
-        100% {
-          border-color: red;
-        }
-      }
-      .login {
-        text-align: center;
-        align-content: center;
-        align-items: center;
-        border: 5px solid;
-        animation: rgb-border 3s infinite;
-        padding: 0px; 
-        border-radius: 10px;
-        background-color: rgb(28, 209, 140);
-        height: 15cm;    
-        margin-inline: 10cm;
-        }
-    </style>
 </head>
 <header>
     <a href="index.html">
@@ -117,18 +79,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </a>
 </header>
 <body>
-   <form class="login" action="login.php" method="post">
-    <H2>Login</H2>
-    <div class="form-floating mb-3" >
-        <input type="email" class="form-control" id="floatingInput" placeholder="" name="email">
-        <label  for="floatingInput"> Email </label>
-      </div>
-      <div class="form-floating">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password">
-        <label for="floatingPassword">Password</label>
-      </div>
-      <br>
-        <input type="submit" value="Login">
-</form>
+<div class="Login">
+        <form action="login.php" method="post" ><!--anexamos etiqueta form-->
+            <!--Texto o titulo-->
+            <h1>Login</h1> 
+            <!--etiqueta que guardara el usuario-->
+            <label>Email</label>
+            <input type="email" name="email">
+           <!--etiqueta que guardara la contraseña-->
+            <label>Password</label>
+            <input type="password" name="password">
+           <!--etiqueta para hacer el boton-->
+           
+            <button>Login</button>
+
+        </form> 
+    </div><!--terminamos etiqueta div-->
 </body>
 </html>
